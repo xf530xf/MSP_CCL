62227c33eabb77767be819eefc36a580ba46fe47
xf530xf <62996609+xf530xf@users.noreply.github.com>
2025-06-03 00:22:09 +0800

    Add files via upload

---
+++ model/semseg/deeplabv3plus.py
@@ -0,0 +1,174 @@
+import model.backbone.resnet as resnet
+from model.backbone.xception import xception
+from .resnet import *
+import torch
+from torch import nn
+import torch.nn.functional as F
+from functools import partial
+#from timm.models import create_model, resume_checkpoint, convert_splitbn_model
+#############################################################################
+class Disentangler(nn.Module):
+    def __init__(self, cin):
+        super(Disentangler, self).__init__()
+
+        self.activation_head = nn.Conv2d(cin, 1, kernel_size=3, padding=1, bias=False)
+        self.bn_head = nn.BatchNorm2d(1)
+
+    def forward(self, x):
+        N, C, H, W = x.size()
+        ccam = torch.sigmoid(self.bn_head(self.activation_head(x)))
+
+        ccam_ = ccam.reshape(N, 1, H * W)                          # [N, 1, H*W]
+        x = x.reshape(N, C, H * W).permute(0, 2, 1).contiguous()   # [N, H*W, C]
+        fg_feats = torch.matmul(ccam_, x) / (H * W)                # [N, 1, C]
+        bg_feats = torch.matmul(1 - ccam_, x) / (H * W)            # [N, 1, C]
+
+        return fg_feats.reshape(x.size(0), -1), bg_feats.reshape(x.size(0), -1), ccam
+
+#############################################################################
+
+
+
+
+
+
+
+##############################################################################
+class DeepLabV3Plus(nn.Module):
+    def __init__(self, cfg):
+        super(DeepLabV3Plus, self).__init__()
+
+        if 'resnet' in cfg['backbone']:
+            self.backbone = resnet.__dict__[cfg['backbone']](pretrained=True, 
+                                                          replace_stride_with_dilation=cfg['replace_stride_with_dilation'])
+
+
+        else :
+            assert cfg['backbone'] == 'xception'
+            self.backbone = xception(pretrained=True)
+
+        low_channels = 256
+        high_channels = 2048
+
+        self.head = ASPPModule(high_channels, cfg['dilations'])
+
+        self.reduce = nn.Sequential(nn.Conv2d(low_channels, 48, 1, bias=False),
+                                    nn.BatchNorm2d(48),
+                                    nn.ReLU(True))
+
+        self.fuse = nn.Sequential(nn.Conv2d(high_channels // 8 + 48, 256, 3, padding=1, bias=False),
+                                  nn.BatchNorm2d(256),
+                                  nn.ReLU(True),
+                                  nn.Conv2d(256, 256, 3, padding=1, bias=False),
+                                  nn.BatchNorm2d(256),
+                                  nn.ReLU(True))
+
+        self.classifier = nn.Conv2d(256, cfg['nclass'], 1, bias=True)
+
+        ############################################################
+        # self.conv1 = model_back.conv1
+        # self.bn1 = model_back.bn1
+        # self.relu = model_back.relu
+        # self.maxpool = model_back.maxpool
+        # self.layer1 = model_back.layer1
+        # self.layer2 = model_back.layer2
+        # self.layer3 = model_back.layer3
+        # self.layer4 = model_back.layer4
+        # self.ac_head = Disentangler(cin=3072)
+        #############################################################
+
+
+    def forward(self, x, need_fp=False):
+        h, w = x.shape[-2:]
+        #######################################
+        # xq = self.conv1(x)
+        # xq = self.bn1(xq)
+        # xq = self.relu(xq)
+        # xq = self.maxpool(xq)
+        # xq = self.layer1(xq)
+        # xq = self.layer2(xq)
+        # x1= self.layer3(xq)
+        # x2 = self.layer4(x1)
+        # x_out=torch.cat([x2, x1], dim=1)
+        # fg_feats, bg_feats, ccam = self.ac_head(x_out)
+        ######################################
+
+        feats = self.backbone.base_forward(x)
+
+        c1, c4 = feats[0], feats[-1]
+
+        if need_fp:
+            outs = self._decode(torch.cat((c1, nn.Dropout2d(0.5)(c1))),
+                                torch.cat((c4, nn.Dropout2d(0.5)(c4))))
+            outs = F.interpolate(outs, size=(h, w), mode="bilinear", align_corners=True)
+            out, out_fp = outs.chunk(2)
+
+            return out, out_fp
+
+        out = self._decode(c1, c4)
+        out = F.interpolate(out, size=(h, w), mode="bilinear", align_corners=True)
+
+        return out
+
+    def _decode(self, c1, c4):
+        c4 = self.head(c4)
+        c4 = F.interpolate(c4, size=c1.shape[-2:], mode="bilinear", align_corners=True)
+
+        c1 = self.reduce(c1)
+
+        feature = torch.cat([c1, c4], dim=1)
+        feature = self.fuse(feature)
+
+        out = self.classifier(feature)
+
+        return out
+
+
+def ASPPConv(in_channels, out_channels, atrous_rate):
+    block = nn.Sequential(nn.Conv2d(in_channels, out_channels, 3, padding=atrous_rate,
+                                    dilation=atrous_rate, bias=False),
+                          nn.BatchNorm2d(out_channels),
+                          nn.ReLU(True))
+    return block
+
+
+class ASPPPooling(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ASPPPooling, self).__init__()
+        self.gap = nn.Sequential(nn.AdaptiveAvgPool2d(1),
+                                 nn.Conv2d(in_channels, out_channels, 1, bias=False),
+                                 nn.BatchNorm2d(out_channels),
+                                 nn.ReLU(True))
+
+    def forward(self, x):
+        h, w = x.shape[-2:]
+        pool = self.gap(x)
+        return F.interpolate(pool, (h, w), mode="bilinear", align_corners=True)
+
+
+class ASPPModule(nn.Module):
+    def __init__(self, in_channels, atrous_rates):
+        super(ASPPModule, self).__init__()
+        out_channels = in_channels // 8
+        rate1, rate2, rate3 = atrous_rates
+
+        self.b0 = nn.Sequential(nn.Conv2d(in_channels, out_channels, 1, bias=False),
+                                nn.BatchNorm2d(out_channels),
+                                nn.ReLU(True))
+        self.b1 = ASPPConv(in_channels, out_channels, rate1)
+        self.b2 = ASPPConv(in_channels, out_channels, rate2)
+        self.b3 = ASPPConv(in_channels, out_channels, rate3)
+        self.b4 = ASPPPooling(in_channels, out_channels)
+
+        self.project = nn.Sequential(nn.Conv2d(5 * out_channels, out_channels, 1, bias=False),
+                                     nn.BatchNorm2d(out_channels),
+                                     nn.ReLU(True))
+
+    def forward(self, x):
+        feat0 = self.b0(x)
+        feat1 = self.b1(x)
+        feat2 = self.b2(x)
+        feat3 = self.b3(x)
+        feat4 = self.b4(x)
+        y = torch.cat((feat0, feat1, feat2, feat3, feat4), 1)
+        return self.project(y)
